8f068a3044d4b1f28a1643772a3c66075890febb
Allen John <allenjohnmonapallil@gmail.com>
2025-03-26 13:07:32 +0530

    remove: config/corsOptions.ts

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,9 @@ import corsOptions from './config/corsOptions';
 import express, { Application } from "express";
 import router from "./routes";
 import auth from "./routes/auth";
-import dotenv from "dotenv";
-import corsOptions from "./config/corsOptions";
-dotenv.config();
 
 // Initialize the express app.
 const app: Application = express();
-
-// Enable CORS
-app.use(corsOptions);
 
 // Middleware setup.
 app.use(express.json());
